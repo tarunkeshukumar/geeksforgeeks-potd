2f93ed9af9396d259db7e036115bfb6ae3b91c27
Tarun Kumar <45817243+tarunkeshukumar@users.noreply.github.com>
2023-07-16 23:26:43 +0530

    Create Queue Reversal.cpp

---
+++ Queue Reversal.cpp
@@ -0,0 +1,21 @@
+//https://practice.geeksforgeeks.org/problems/queue-reversal/1
+
+//Function to reverse the queue.
+class Solution
+{
+    public:
+    queue<int> rev(queue<int> q)
+    {
+        // add code here.
+        stack<int>s;
+        while(!q.empty()){
+            s.push(q.front());
+            q.pop();
+        }
+        while(!s.empty()){
+            q.push(s.top());
+            s.pop();
+        }
+        return q;
+    }
+};
