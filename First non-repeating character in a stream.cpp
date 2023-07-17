5067a28a1c1636f660ca2540910c1d8cb7050d64
Tarun Kumar <45817243+tarunkeshukumar@users.noreply.github.com>
2023-07-17 23:51:55 +0530

    Create First non-repeating character in a stream.cpp

---
+++ First non-repeating character in a stream.cpp
@@ -0,0 +1,38 @@
+// https://practice.geeksforgeeks.org/problems/first-non-repeating-character-in-a-stream1216/1
+
+class Solution {
+	public:
+		string FirstNonRepeating(string A){
+		    // Code here
+		    unordered_map<char,int> count;
+            queue<int> q;
+            string ans = "";
+            
+            for(int i=0;i<A.length();i++)
+            {
+                char ch = A[i];
+                
+                //increase count
+                count[ch]++;
+                //push in the queue
+                q.push(ch);
+                
+                while(!q.empty())
+                {
+                    if(count[q.front()]>1){
+                        //repeating character
+                        q.pop();
+                    }
+                    else
+                    {
+                        //non repeating character
+                        ans.push_back(q.front()); 
+                        break;
+                    }
+                }
+                if(q.empty())
+                    ans.push_back('#');
+            }
+            return ans;
+		}
+};
